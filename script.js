0cc58c998abc05f1ed2616de2daf330046284ebd
Nike <198646766+BlakKMango@users.noreply.github.com>
2025-10-22 16:11:10 +0100

    Introduce rounds, score counting, and best of 5.

--- script.js
+++ script.js
@@ -1,51 +1,88 @@
-// Pseudo Code
-
-// 1. User is given instructions on how to play the game. 
-// 2. User inputs one of the following options: Rock, Paper, or Scissors
-// 3. The computer randomly generates one of the following options: Rock, Paper, or Scissors.
-// 4. Rock > Scissors > Paper > Rock
-// 5. Announce the winner.
+// Prompt the user to enter a choice
+// return the user and computer's choice
+// Assign a winner
+// Track the score
 
 
-//Prompt the user to enter their Choice
-function getHumanChoice() {
-    let randomLetter = prompt("Enter 'r' for rock, 'p' for paper, or 's' for scissors. Go!");
-    let humChoice;
+//VARIABLES
+let humanScore = 0
+let computerScore = 0
+let humanChoice;
+let computerChoice;
+let winner;
 
-    if (randomLetter.toLowerCase() == "r") {
-        humChoice = "Rock"
-    } else if (randomLetter.toLowerCase() == "p") {
-        humChoice = "Paper"
-    } else if (randomLetter.toLowerCase() == "s") {
-        humChoice = "Scissors";
-    } else humChoice = "Hmm, I don't recognise that choice";
-
-    return humChoice;
-}
-
-
-console.log(getHumanChoice())
-
-// Get a random number between 1 and 3.
-// Convert the number into rock, paper, or scissors.
+//FUNCTIONS
+//Randomly choose one: Rock, Paper, or Scissors and show choice
 function getComputerChoice(){
-    function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min
+    let randomNumber = Math.random() * (3 - 0) + 0;
+    if (randomNumber < 1){
+        computerChoice = "Rock";
+    } else if (randomNumber >= 1 && randomNumber < 2){
+        computerChoice = "Paper";
+    } else if (randomNumber >= 2 && randomNumber < 3) {
+        computerChoice = "Scissors";
     }
-
-    let randomNumber = getRandomNumber(0, 3);
-    let compChoice;
-
-    if (randomNumber < 1) {
-        compChoice = "Rock";
-    } else if (randomNumber >= 1 && randomNumber < 2) {
-        compChoice = "Paper";
-    } else {
-        compChoice = "Scissors";
+    console.log("Computer chose " + computerChoice)
+    return computerChoice
+}
+// Prompt the user to randomly choose one: Rock, Paper, or Scissors and show choice
+function getHumanChoice(){
+    let randomLetter = prompt("Enter 'r' for rock, 'p' for paper, or 's' for scissors. Go!").toLowerCase()
+    if (randomLetter == "r"){
+        humanChoice = "Rock";
+    } else if (randomLetter == "p"){
+        humanChoice = "Paper";
+    } else if (randomLetter == "s"){
+        humanChoice = "Scissors";
     }
-    
-    return compChoice; 
+    console.log("You chose " + humanChoice)
+    return humanChoice
 }
 
-console.log(getComputerChoice())
+//Show who the winner is in the console
+function decideWinner(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log("It's a draw.");
+        winner = null
+    } else if ((humanChoice === "Rock" && computerChoice === "Scissors") 
+        || (humanChoice === "Scissors" && computerChoice === "Paper")
+        || (humanChoice === "Paper" && computerChoice === "Rock")){
+        winner = "human";
+        console.log("You win!");
+    } else {
+        winner = "computer";
+        console.log("Computer wins!")
+    }
+    return winner;
+}
 
+//Play a single round
+function playRound(){
+    getHumanChoice()
+    getComputerChoice()
+    decideWinner(humanChoice, computerChoice)
+    //Add a point to the winner's score
+    if (winner === "computer"){
+        computerScore++
+    } else if (winner === "human"){
+        humanScore++
+    }
+
+    console.log("Score: You = " + humanScore + ". Computer =" + computerScore)
+    return computerScore, humanScore
+}
+
+function playBestOfFive(){
+    while ((computerScore + humanScore) < 5){
+        playRound();
+    }
+    if (humanScore > computerScore){
+        console.log("Well done. Another victory for humanity.");
+    } else {
+        console.log("Better luck next time human.")
+    }
+}
+
+
+//Play the game!
+playBestOfFive()
