476efc2906ef62b13ea51e43dc1a120c2afa3898
Nike <198646766+BlakKMango@users.noreply.github.com>
2025-11-26 07:23:53 +0000

    Make play again button restart the game
    The play again button now restarts the game when clicked. Users will return back to the start game screen.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const inGameScreen = document.querySelector("#human-choices")
 const humanChoiceButton = document.querySelectorAll(".human-choices-button");
 const gameOverScreen = document.querySelector("#game-over-screen")
 const overallWinner = document.querySelector("#who-won")
+const resultScreen = document.querySelector("#result-screen")
+const replayButton = document.querySelector("#replay-button");
 
 const gamestate = {
     humanScore: 0,
@@ -21,6 +23,16 @@ function loadGameStartState() {
         "color: white; background-color: black; width: 200px; height: 100px; border-radius: 20px");
     gameStartButton.textContent = "Play"
     startGameScreen.appendChild(gameStartButton)
+    startGameScreen.style.display = "flex";
+    inGameScreen.style.display = "none";
+    gameOverScreen.style.display = "none";
+    resultScreen.style.display = "none";
+}
+
+function startGame(){
+    startGameScreen.setAttribute("style", "display: none")
+    gameOverScreen.setAttribute("style", "display: none")
+    inGameScreen.setAttribute("style", "display: flex")
 }
 
 function playRound(event) {
@@ -35,12 +47,6 @@ function playRound(event) {
     if (gamestate.clickCount === 5){
         gameOver()
     }
-}
-
-function startGame(){
-    startGameScreen.setAttribute("style", "display: none")
-    gameOverScreen.setAttribute("style", "display: none")
-    inGameScreen.setAttribute("style", "display: flex")
 }
 
 function getHumanChoice(event) {
@@ -110,8 +116,11 @@ function gameOver(){
 //===DOM MANIPULATION===//
 
 document.addEventListener("DOMContentLoaded", loadGameStartState)
+
 gameStartButton.addEventListener("click", startGame)
 
 humanChoiceButton.forEach(button => {
     button.addEventListener("click", playRound);
 })
+
+replayButton.addEventListener("click", loadGameStartState)
